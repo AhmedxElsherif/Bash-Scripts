094562f7accabac8e2130a4fba98af48a8c4721d
Ahmed ElSherif <us.ahmedelsherif@gmail.com>
2024-12-06 22:11:01 +0200

    Add files via upload
    Translate last copied text from English into Arabic bash script

---
+++ en_ar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------#
+#   File : en_ar.sh                                                      #
+#   Author : Ahmed ElSherif                                              #
+#   Path : Repo/Bash-Scripts                                             #
+#   Description : Translate from EN to AR by Google using Bash Scripting #
+#------------------------------------------------------------------------#
+
+URL="https://translate.google.com.eg/?sl=en&tl=ar&text=$(xclip -o sel)&op=translate"
+firefox "$URL"
